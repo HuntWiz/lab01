6884c0a4303269918c45c42e4363632cfc518d9b
Hunt <botalov.gleb123@gmail.com>
2023-10-03 17:33:31 +0500

    Евклид

---
+++ main.py
@@ -0,0 +1,10 @@
+
+import math
+print(" GCD of two number 0 and 0 is ", math.gcd(0, 0))
+print(" GCD of two number 0 and 48 is ", math.gcd(0, 48))
+a = 60
+b = 48
+print(" GCD of two number 60 and 48 is ", math.gcd(a, b))
+print(" GCD of two number 48 and -12 is ", math.gcd(48, -12))
+print(" GCD of two number -24 and -18 is ", math.gcd(-24, -18))
+print(" GCD of two number -60 and 48 is ", math.gcd(-60, 48))
